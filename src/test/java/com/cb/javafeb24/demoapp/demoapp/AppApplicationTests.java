3ac3449482c3864be5b5ca78ef0adaacbf19be2d
DinaFadil <nurdinafadil@gmail.com>
2024-02-22 12:22:34 +0800

    220224

---
+++ src/test/java/com/cb/javafeb24/demoapp/demoapp/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cb.javafeb24.demoapp.demoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
